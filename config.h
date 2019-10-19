6f60cb06e76d8f30b9764ff75205e66ade98028e
Michael Gebetsroither <michael@mgeb.org>
2019-10-19 18:40:07 +0200

    config: set default cols/rows

--- config.h
+++ config.h
@@ -139,8 +139,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 160;
+static unsigned int rows = 48;
 
 /*
  * Default colour and shape of the mouse cursor
